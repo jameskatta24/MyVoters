3fcd7afade7529cc10fbc6dabd831f320eb1871a
jameskatta24 <jameskatta24@gmail.com>
2018-12-04 15:13:09 +0530

    Abcd........commited

--- Voters/src/main/java/com/app/test/Abcd.java
+++ Voters/src/main/java/com/app/test/Abcd.java
@@ -1,10 +1,12 @@
 package com.app.test;
 
+import com.app.model.Employee;
+
 public class Abcd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+  Employee e1=new Employee();
+  System.out.println("employee");
 	}
 
 }
