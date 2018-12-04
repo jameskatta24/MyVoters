d9a3ddd82e48aef447342e87214d17a074a497ff
Vinay <Vinay@DESKTOP-6SF1QJ0>
2018-12-04 14:59:20 +0530

    employee class modified compare

--- Voters/src/main/java/com/app/model/Employee.java
+++ Voters/src/main/java/com/app/model/Employee.java
@@ -3,20 +3,5 @@ package com.app.model;
 public class Employee {
 private int eId;
 private String eName;
-public Employee() {
-	super();
-}
-public int geteId() {
-	return eId;
-}
-public void seteId(int eId) {
-	this.eId = eId;
-}
-public String geteName() {
-	return eName;
-}
-public void seteName(String eName) {
-	this.eName = eName;
-}
-	
+
 }
