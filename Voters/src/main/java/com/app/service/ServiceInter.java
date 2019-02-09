c959c6258005734df6afac1d7deff1dde58e8d0b
jameskatta24 <jameskatta24@gmail.com>
2019-02-10 00:34:09 +0530

    in service save method is added

--- Voters/src/main/java/com/app/service/ServiceInter.java
+++ Voters/src/main/java/com/app/service/ServiceInter.java
@@ -1,5 +1,11 @@
 package com.app.service;
 
-public class ServiceInter {
+import com.app.model.Employee;
 
+public class ServiceInter {
+	public void save(Employee e) {
+		Employee e1 = new Employee();
+		e1.seteId(10);
+		e1.seteName("Katta");
+	}
 }
