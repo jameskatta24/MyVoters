017fcd9c3d03a495a3d41ea63b865d86d31f1fd1
Kattauda <udaykumar1940@gmail.com>
2019-02-09 23:51:54 +0530

    setters and getters added to Employee

--- Voters/src/main/java/com/app/model/Employee.java
+++ Voters/src/main/java/com/app/model/Employee.java
@@ -4,4 +4,19 @@ public class Employee {
 private int eId;
 private String eName;
 
+public int geteId() {
+	return eId;
+}
+public void seteId(int eId) {
+	this.eId = eId;
+}
+public String geteName() {
+	return eName;
+}
+public void seteName(String eName) {
+	this.eName = eName;
+}
+
+
+
 }
