4275e04b9701e3b1ff49bfd488c22c67d79f3d05
jameskatta <jameskatta24@gmail.com>
2019-02-10 00:19:18 +0530

    Nothing is added

--- Voters/src/main/java/com/app/model/Employee.java
+++ Voters/src/main/java/com/app/model/Employee.java
@@ -20,33 +20,6 @@ public void seteName(String eName) {
 public String toString() {
 	return "Employee [eId=" + eId + ", eName=" + eName + "]";
 }
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + eId;
-	result = prime * result + ((eName == null) ? 0 : eName.hashCode());
-	return result;
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Employee other = (Employee) obj;
-	if (eId != other.eId)
-		return false;
-	if (eName == null) {
-		if (other.eName != null)
-			return false;
-	} else if (!eName.equals(other.eName))
-		return false;
-	return true;
-}
-
 
 
 
