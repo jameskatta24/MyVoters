621286c72ec670be88924fa185aede185a8e21c7
james <james@gmail.com>
2019-02-10 00:43:14 +0530

    in service user object is added

--- Voters/src/main/java/com/app/service/ServiceInter.java
+++ Voters/src/main/java/com/app/service/ServiceInter.java
@@ -1,12 +1,19 @@
 package com.app.service;
 
 import com.app.model.Employee;
+import com.app.model.User;
 
 public class ServiceInter {
 	public void save(Employee e) {
+		User u = new User();
+		u.setId(1);
+		u.setName("mani");
+		System.out.println(u);
+
 		Employee e1 = new Employee();
 		e1.seteId(20);
 		e1.seteName("gatta");
 		System.out.println(e1);
+
 	}
 }
